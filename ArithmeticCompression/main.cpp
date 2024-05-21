1ecb49549a35718d42bb64e860236660563df179
liza <liza.serpikova@stud.tsu.ru>
2024-05-21 22:23:12 +0700

    Открытие файлов для работы с ними

--- ArithmeticCompression/main.cpp
+++ ArithmeticCompression/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-   
+double make_code(std::ifstream &inp, std::ofstream &out) {
+    if (!inp.is_open()) {
+        std::cout << "Ошибка открытия файла!" << "\n";
+        return 0.0;
+    }
+    
+    inp.close();
+    out.close();
+    
+    return 1.0;
+}
+
+int main() {
+    std::ifstream inp("file.txt", std::ios::out | std::ios::binary);
+    std::ofstream out("out_file.txt", std::ios::out | std::ios::binary);
+    
+    double value = make_code(inp, out);
+    
+    std::cout << value << "\n";
+    
     return 0;
 }
